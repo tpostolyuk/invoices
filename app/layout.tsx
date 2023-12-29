e4b0385106da4c2e274193d8387437d0a67cf2d1
GreenOps <greenops@greenops.io>
2023-12-29 15:09:25 +0200

    feat: setup main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
-import { Inter } from "next/font/google";
-import type { Metadata } from "next";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Invoices",
-  description: "Easy invoice management app",
-};
+import { inter } from "@/app/ui/fonts";
+import "@/app/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
